a662ab340e604621862394d16bdf408aa00935ef
Gustavo Cotta <gmcotta34@gmail.com>
2020-11-09 15:49:48 -0300

    Subscribing and unsubscribing cityWeather variable on home page

--- src/app/pages/home/containers/home/home.page.ts
+++ src/app/pages/home/containers/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Bookmark } from 'src/app/shared/models/bookmark.model';
 import { CityWeather } from 'src/app/shared/models/weather.model';
 
 import * as fromHomeActions from '../../store/home.actions';
@@ -12,27 +14,46 @@ import * as fromHomeSelectors from '../../store/home.selectors';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss']
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   searchControl: FormControl;
   text: string;
-  cityWeather$: Observable<CityWeather>;
+  cityWeather: CityWeather;
   loading$: Observable<boolean>;
   error$: Observable<boolean>;
+
+  private componentDestroyed$ = new Subject();
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
     this.searchControl = new FormControl('', Validators.required);
-    this.cityWeather$ = this.store
-      .pipe(select(fromHomeSelectors.selectCurrentWeather));
+    this.store
+      .pipe(
+        select(fromHomeSelectors.selectCurrentWeather),
+        takeUntil(this.componentDestroyed$),
+      )
+      .subscribe({ next: (value: CityWeather) => this.cityWeather = value });
     this.loading$ = this.store
       .pipe(select(fromHomeSelectors.selectCurrentWeatherLoading));
     this.error$ = this.store
       .pipe(select(fromHomeSelectors.selectCurrentWeatherError));
   }
 
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.unsubscribe();
+  }
+
   doSearch(): void {
     const query = this.searchControl.value;
     this.store.dispatch(fromHomeActions.loadCurrentWeather({ query }));
+  }
+
+  onToggleBookmark() {
+    const bookmark = new Bookmark();
+    bookmark.id = this.cityWeather.city.id;
+    bookmark.name = this.cityWeather.city.name;
+    bookmark.country = this.cityWeather.city.country;
+    bookmark.coord = this.cityWeather.city.coord;
   }
 }
