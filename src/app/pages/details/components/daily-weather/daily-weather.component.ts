e6c327de4fef76e1790f9915eb11755e546cd8f1
Gustavo Cotta <gmcotta34@gmail.com>
2020-11-10 17:44:02 -0300

    Adding date-fns and formatting date on daily weather component

--- src/app/pages/details/components/daily-weather/daily-weather.component.ts
+++ src/app/pages/details/components/daily-weather/daily-weather.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { format } from 'date-fns';
+import fromUnixTime from 'date-fns/fromUnixTime';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { DailyWeather, Weather } from 'src/app/shared/models/weather.model';
 
@@ -20,10 +23,16 @@ export class DailyWeatherComponent {
   }
 
   get date(): string {
-    return this.dailyWeather.date.toString();
+    return format(fromUnixTime(this.dailyWeather.date), "dd MMM - EEEE", {
+      locale: ptBR,
+    });
   }
 
   get icon(): string {
     return `http://openweathermap.org/img/wn/${this.weather.icon}@2x.png`;
+  }
+
+  unixToHourMinute(unixValue: number): string {
+    return format(fromUnixTime(unixValue), "HH:mm");
   }
 }
